146cc282667dcae01fd4edde566261edd878b3c7
ahram2 <helloahram1@gmail.com>
2025-07-10 15:58:42 +0900

    Change Format

--- src/main_page_test_case.py
+++ src/main_page_test_case.py
@@ -2,7 +2,8 @@ import time
 from pages.pages import page_v1
 from playwright.sync_api import sync_playwright
 
-def check_main_page(url:str):
+
+def check_main_page(url: str):
     try:
         with sync_playwright() as playwright:
             page = page_v1(playwright)
@@ -12,11 +13,12 @@ def check_main_page(url:str):
             # install 4 carrier to ports
             page.close()
 
-    finally:        
+    finally:
         pass
         # 해당 method 종료 시 무조건 실행하는 기능
 
-def check_web_title(url:str):
+
+def check_web_title(url: str):
     try:
         with sync_playwright() as playwright:
             page = page_v1(playwright)
@@ -28,6 +30,7 @@ def check_web_title(url:str):
     finally:
         pass
 
+
 def check_weather(url: str):
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch(headless=False)
@@ -37,14 +40,17 @@ def check_weather(url: str):
             page.goto(url)
             page.get_by_role("button", name="바로가기 펼침").click()
             with page.expect_popup() as page1_info:
-                page.get_by_label("주요 서비스").get_by_role("link", name="날씨").click()
+                page.get_by_label("주요 서비스").get_by_role(
+                    "link", name="날씨"
+                ).click()
             page1 = page1_info.value
 
             # 검증: 페이지 타이틀에 "날씨" 포함
             assert "날씨" in page1.title()
-            
+
         finally:
             browser.close()
+
 
 def check_news(url: str):
     with sync_playwright() as playwright:
@@ -55,12 +61,13 @@ def check_news(url: str):
             page.goto(url)
             page.get_by_role("button", name="바로가기 펼침").click()
             with page.expect_popup() as page1_info:
-                page.get_by_label("주요 서비스").get_by_role("link", name="뉴스").click()
+                page.get_by_label("주요 서비스").get_by_role(
+                    "link", name="뉴스"
+                ).click()
             page1 = page1_info.value
 
             # 검증: 페이지 타이틀에 "뉴스" 포함
             assert "뉴스" in page1.title()
-            
+
         finally:
             browser.close()
-
